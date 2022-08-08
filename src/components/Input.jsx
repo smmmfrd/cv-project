abaa6c1a2d61df26f4b7c37b317e30700cc4a528
Sam Mumford <smmmfrd@gmail.com>
2022-08-08 14:07:27 -0700

    Implement Cycle Between Inputs

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,41 @@
 import PersonalDetails from "./PersonalDetails";
 import Work from "./Work/Work";
 import Education from "./Education/Education";
+import { useState } from "react";
 export default function Input(props){
+
+    const [section, setSection] = useState(0);
+
+    function handleChange(val){
+        var newSection = section + val;
+        if(newSection >= 3) {
+            newSection = 0;
+        } else if(newSection < 0){
+            newSection = 2;
+        }
+        setSection(newSection);
+    }
+
+    function renderSwitch(){
+        switch (section) {
+            case 0:
+                return <PersonalDetails handleChange={props.handleChange} data={props.data} />
+                break;
+            case 1:
+                return <Work handleChange={props.handleChange} experience={props.data.experience}/>
+                break;
+            case 2:
+                return <Education handleChange={props.handleChange} education={props.data.education}/>
+                break;
+        }
+    }
+
     return(
         <div className="cv--input-container">
-            {/* <PersonalDetails handleChange={props.handleChange} data={props.data} /> */}
-            {/* <Work handleChange={props.handleChange} experience={props.data.experience}/> */}
-            <Education handleChange={props.handleChange} education={props.data.education}/>
+            {renderSwitch()}
+            <button onClick={() => handleChange(-1)}>Previous Section</button>
+            <button onClick={() => handleChange(1)}>Next Section</button>
+            <p>{section}</p>
         </div>
     )
 }
